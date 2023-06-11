67d692e1c40843efe4970c88f455f192f8582335
Hanieh Ghassemi <89027450+HaniehGhassemi@users.noreply.github.com>
2023-06-11 21:18:07 +0330

    Add: calculate histogram vector

--- index.py
+++ index.py
@@ -1,10 +1,23 @@
 import cv2
 import numpy as np
-img = cv2.imread('home.png', 0)
-gamma_values = [0.5,0.6,0.7,0.8,1.2,1.3]
-for gamma in gamma_values:
-    gamma_img = np.power(img/255.0, gamma)
-    cv2.imshow('Gamma = ' + str(gamma), gamma_img)
-    cv2.waitKey(0)
+import matplotlib.pyplot as plt
 
-cv2.destroyAllWindows()
+def calc_histogram(image):
+    hist, _ = np.histogram(image.ravel(), bins=256, range=[0, 256])
+    hist = hist / np.sum(hist)
+    return hist
+
+def draw_histogram(hist):
+    bins = np.arange(257)
+    fig, ax = plt.subplots()
+    ax.set_xlabel('Pixel intensity')
+    ax.set_ylabel('Frequency')
+    ax.set_title('Grayscale Image Histogram')
+    ax.bar(bins[:-1], hist, width=1)
+    plt.show()
+
+image = cv2.imread('home.png', cv2.IMREAD_GRAYSCALE)
+
+hist = calc_histogram(image)
+
+draw_histogram(hist)
