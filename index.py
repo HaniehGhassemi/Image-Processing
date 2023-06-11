ec63f65953312b32519255a75912efa439962cef
Hanieh Ghassemi <89027450+HaniehGhassemi@users.noreply.github.com>
2023-06-11 20:44:36 +0330

    add: count gama of black and white picture

--- index.py
+++ index.py
@@ -1,7 +1,7 @@
 import cv2
 import numpy as np
-img = cv2.imread('rice.png', 0)
-gamma_values = [0.1, 0.2, 0.8, 1.5, 3.5]
+img = cv2.imread('home.png', 0)
+gamma_values = [0.5,0.6,0.7,0.8,1.2,1.3]
 for gamma in gamma_values:
     gamma_img = np.power(img/255.0, gamma)
     cv2.imshow('Gamma = ' + str(gamma), gamma_img)
