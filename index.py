206576d955ee7e6c50a6a8494d9a38fb991bbe4a
Hanieh Ghassemi <89027450+HaniehGhassemi@users.noreply.github.com>
2023-06-16 21:13:14 +0330

    Add: histogram

--- index.py
+++ index.py
@@ -1,57 +1,59 @@
 import cv2
 import numpy as np
-import matplotlib.pyplot as plt
 
-def calc_histogram(image):
-    hist, _ = np.histogram(image.ravel(), bins=256, range=[0, 256])
-    hist = hist / np.sum(hist)
-    return hist
+def automatic_gamma_correction(image, gamma=1.0):
+    # Step 2: Divide the image into smaller blocks
+    block_size = 16
+    h, w = image.shape[:2]
+    blocks_v = h // block_size
+    blocks_h = w // block_size
+    
+    # Step 3: Calculate the mean and variance of each block
+    block_means = np.zeros((blocks_v, blocks_h))
+    block_variances = np.zeros((blocks_v, blocks_h))
+    for i in range(blocks_v):
+        for j in range(blocks_h):
+            block = image[i*block_size:(i+1)*block_size, j*block_size:(j+1)*block_size]
+            block_means[i, j] = np.mean(block)
+            block_variances[i, j] = np.var(block)
 
-def calc_equalizer(hist):
-    cdf = np.cumsum(hist)
-    equalizer = np.round(cdf * 255).astype(int)
-    return equalizer
+    # Step 4: Divide the blocks into dark and bright sets based on their mean
+    dark_blocks = []
+    bright_blocks = []
+    for i in range(blocks_v):
+        for j in range(blocks_h):
+            if block_means[i, j] < 128:
+                dark_blocks.append((i, j))
+            else:
+                bright_blocks.append((i, j))
 
-# بارگذاری تصویر
-image_path = 'home.png'
-image = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
+    # Step 5: Calculate the mean and variance of dark and bright sets
+    dark_var = np.mean(block_variances[[i for i, j in dark_blocks], [j for i, j in dark_blocks]])
+    bright_var = np.mean(block_variances[[i for i, j in bright_blocks], [j for i, j in bright_blocks]])
 
-# بررسی موفقیت بارگذاری تصویر
-if image is None:
-    print(f"Could not read the image at path {image_path}")
-else:
-    # محاسبه هیستوگرام تصویر
-    hist = calc_histogram(image)
+    # Step 6: Determine the gamma correction value based on the variances
+    if dark_var < bright_var:
+        gamma = gamma * np.sqrt(bright_var / dark_var)
+    else:
+        gamma = gamma * np.sqrt(dark_var / bright_var)
 
-    # محاسبه تابع هموارسازی
-    equalizer = calc_equalizer(hist)
+    # Apply gamma correction to the image
+    corrected = np.power(image / 255.0, gamma) * 255.0
+    corrected = corrected.astype(np.uint8)
 
-    # هموارسازی تصویر
-    equalized_image = equalizer[image]
+    return corrected, gamma
 
-    # نمایش تصویر و هیستوگرام‌ها
-    fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(10, 5))
-    ax1.imshow(image, cmap='gray')
-    ax1.set_title('Original Image')
-    ax2.imshow(equalized_image, cmap='gray')
-    ax2.set_title('Equalized Image')
-    plt.show()
+# Load the image
+image = cv2.imread('rice.png', cv2.IMREAD_GRAYSCALE)
 
-    fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(10, 5))
-    ax1.bar(np.arange(256), hist, width=1)
-    ax1.set_xlabel('Pixel intensity')
-    ax1.set_ylabel('Frequency')
-    ax1.set_title('Original Image Histogram')
-    ax2.bar(np.arange(256), calc_histogram(equalized_image), width=1)
-    ax2.set_xlabel('Pixel intensity')
-    ax2.set_ylabel('Frequency')
-    ax2.set_title('Equalized Image Histogram')
-    plt.show()
+# Apply automatic gamma correction with gamma = 1.0
+corrected, gamma = automatic_gamma_correction(image, gamma=1.0)
 
-    # نمایش تابع هموارسازی
-    fig, ax = plt.subplots()
-    ax.plot(np.arange(256), calc_equalizer(hist))
-    ax.set_xlabel('Pixel intensity')
-    ax.set_ylabel('Equalizer function value')
-    ax.set_title('Histogram Equalization Function')
-    plt.show()
+# Display the original and corrected images
+cv2.imshow('Original', image)
+cv2.imshow('Corrected', corrected)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+# Print the gamma value
+print('Gamma:', gamma)
